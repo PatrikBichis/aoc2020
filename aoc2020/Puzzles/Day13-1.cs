2727eb13a2c10f58f8cc3f07dfad3dcef836125d
Patrik Olsson Bichis <patrik.olsson@x-firm.com>
2020-12-17 00:56:01 +0100

    Day 13

--- aoc2020/Puzzles/Day13-1.cs
+++ aoc2020/Puzzles/Day13-1.cs
@@ -16,10 +16,41 @@ namespace aoc2020
 
        public IPuzzel Run()
        {
+            var arriveTime = double.Parse(Input[0]);
+            var parts = Input[1].Split(",");
+            var busses = new List<Bus>();
 
+            foreach(var p in parts)
+            {
+                if(p != "x")
+                {
+                    busses.Add(new Bus() { Id = double.Parse(p) });
+                }
+            }
+
+            foreach(var b in busses)
+            {
+                var count = 1;
+                while(b.ErliestTime < arriveTime)
+                {
+                    b.ErliestTime = b.Id * count;
+                    count++;
+                }
+            }
+
+            var firstBus = busses.OrderBy(x => x.ErliestTime);
+
+            Answer = (firstBus.First().Id * (firstBus.First().ErliestTime - arriveTime)).ToString();
 
            return this;
        }
 
    }
+
+    class Bus
+    {
+        public double Id { get; set; }
+
+        public double ErliestTime { get; set; }
+    }
 } 
--- aoc2020/Puzzles/Day14-1.cs
+++ aoc2020/Puzzles/Day14-1.cs
@@ -16,10 +16,82 @@ namespace aoc2020
 
        public IPuzzel Run()
        {
+            string mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+            var memory = new Dictionary<int, int>();
 
 
-           return this;
+            WriteToMemory(ref memory, 8, ApplyMask(mask, 11));
+            WriteToMemory(ref memory, 7, ApplyMask(mask, 101));
+            WriteToMemory(ref memory, 8, ApplyMask(mask, 0));
+
+            Answer = memory.Sum(x => x.Value).ToString();
+
+
+
+            return this;
        }
 
-   }
+        private void WriteToMemory(ref Dictionary<int,int> memory, int index, int value)
+        {
+            if (memory.ContainsKey(index))
+                memory[index] = value;
+            else
+                memory.Add(index, value);
+        }
+
+        private int ApplyMask(string mask, int value)
+        {
+            value = value.SetBitTo0(1);
+            value = value.SetBitTo1(6);
+
+            return value;
+        }
+
+        static string GetIntBinaryString(int n)
+        {
+            char[] b = new char[36];
+            int pos = 35;
+            int i = 0;
+
+            while (i < 36)
+            {
+                if ((n & (1 << i)) != 0)
+                    b[pos] = '1';
+                else
+                    b[pos] = '0';
+                pos--;
+                i++;
+            }
+            return new string(b);
+        }
+
+    }
+
+    static class BitExtensions
+    {
+        public static int SetBitTo1(this int value, int position)
+        {
+            // Set a bit at position to 1.
+            return value |= (1 << position);
+        }
+
+        public static int SetBitTo0(this int value, int position)
+        {
+            // Set a bit at position to 0.
+            return value & ~(1 << position);
+        }
+
+        public static bool IsBitSetTo1(this int value, int position)
+        {
+            // Return whether bit at position is set to 1.
+            return (value & (1 << position)) != 0;
+        }
+
+        public static bool IsBitSetTo0(this int value, int position)
+        {
+            // If not 1, bit is 0.
+            return !IsBitSetTo1(value, position);
+        }
+    }
+
 } 
