e662c691324861b08aa5872d61895b0be917d209
Patrik Olsson Bichis <patrik.olsson@x-firm.com>
2020-12-18 00:40:25 +0100

    Played with JS

--- aoc2020/Puzzles/Day17-1.cs
+++ aoc2020/Puzzles/Day17-1.cs
@@ -16,10 +16,112 @@ namespace aoc2020
 
        public IPuzzel Run()
        {
+            var grid = new List<Cube>();
 
+            // Extract init grid from input
+            ExtractGridFromInput(Input, ref grid);
+
+            for(int i = 0; i<6; i++)
+            {
+                RunCycle(ref grid);
+            }
+
+            Answer = grid.Count(x => x.IsActive == true).ToString();
 
            return this;
        }
 
+        private void RunCycle(ref List<Cube> grid)
+        {
+            var temp = grid.ToList();
+            foreach(var c in grid)
+            {
+
+            }
+        }
+
+        private int CountActiveNeighbor(ref List<Cube> temp, List<Cube> grid, Cube c)
+        {
+            var count = 0;
+            var x = -1;
+            var y = 0;
+            var z = 0;
+
+            // X ------>
+            // Y
+            // |
+            // |
+            // \/
+
+            CheckNeighbor(ref temp, grid, c, ref count, -1, -1, 0);
+            CheckNeighbor(ref temp, grid, c, ref count, 0, -1, 0);
+            CheckNeighbor(ref temp, grid, c, ref count, -1, 0, 0);
+
+            CheckNeighbor(ref temp, grid, c, ref count, -1, -1, 0);
+            CheckNeighbor(ref temp, grid, c, ref count, 0, -1, -1);
+            CheckNeighbor(ref temp, grid, c, ref count, -1, -1, -1);
+
+            CheckNeighbor(ref temp, grid, c, ref count, 1, 0, 0);
+            CheckNeighbor(ref temp, grid, c, ref count, 0, 1, 0);
+            CheckNeighbor(ref temp, grid, c, ref count, 0, 0, 1);
+
+            CheckNeighbor(ref temp, grid, c, ref count, 1, 1, 0);
+            CheckNeighbor(ref temp, grid, c, ref count, 0, 1, 1);
+            CheckNeighbor(ref temp, grid, c, ref count, 1, 1, 1);
+
+            return count;
+        }
+
+        private void CheckNeighbor(ref List<Cube> temp, List<Cube> grid, Cube c, ref int count, int x, int y, int z)
+        {
+            if (CheckIfNeighborExists(grid, c.X + x, c.Y + y, c.Z + z))
+            {
+                var n = grid.First(i => i.X == c.X + x && i.Y == c.Y + y && i.Z == c.Z + z);
+
+                if (n.IsActive) count++;
+            }
+            else
+            {
+                temp.Add(new Cube { X = c.X + x, Y = c.Y + y, Z = c.Z + z });
+            }
+        }
+
+        private bool CheckIfNeighborExists(List<Cube> grid, int x, int y, int z)
+        {
+            return grid.Exists(i => i.X ==  x && i.Y == y && i.Z == z);
+        }
+
+        private void ExtractGridFromInput(string[] input, ref List<Cube> grid)
+        {
+            var x = 0;
+            var y = 0;
+            foreach (var line in input)
+            {
+                var cubes = line.ToCharArray();
+
+                foreach (var c in cubes)
+                {
+                    x = 0;
+                    var newCube = new Cube { X = x, Y = y, Z = 0 };
+                    newCube.IsActive = c == '.' ? false : true;
+                    grid.Add(newCube);
+                    x++;
+                }
+
+                y++;
+            }
+        }
+       
    }
+
+    class Cube
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+        public bool IsActive { get; set; } = false;
+    }
 } 
---
+++ aoc2020/Puzzles/Day17-1.js
@@ -0,0 +1,86 @@
+﻿var grid = []
+
+function setup() {
+    createCanvas(800, 800, WEBGL);
+
+    cam1 = createCamera();
+    cam1.setPosition(500, -500, 500);
+    cam1.lookAt(0, 0, 0);
+
+    // set variable for previously active camera:
+    currentCamera = 1;
+
+    for (let z = -3; z <= 3; z++) {
+        for (let y = -3; y <= 3; y++) {
+            for (let x = -3; x <= 3; x++) {
+                let active = 0;
+                if (x == 0 && y == -1 && z == 0) active = 1;
+                if (x == 1 && y == 0 && z == 0) active = 1;
+                if (x == -1 && y == 1 && z == 0) active = 1;
+                if (x == 0 && y == 1 && z == 0) active = 1;
+                if (x == 1 && y == 1 && z == 0) active = 1;
+                grid.push([x, y, z, active]);
+            }
+        }
+    }
+
+    print(grid.length)
+    //noLoop();
+}
+
+function draw() {
+    background(200);
+    orbitControl();
+    /*
+        let grid = [
+            [-1, -1, 0, 0],
+            [0, -1, 0, 1],
+            [1, -1, 0, 0],
+            [-1, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 0, 0, 1],
+            [-1, 1, 0, 1],
+            [0, 1, 0, 1],
+            [1, 1, 0, 1],
+    
+        ];
+    */
+    stroke(255, 0, 0);
+    strokeWeight(1);
+    line(-1000, 0, 0, 1000, 0, 0);
+
+    stroke(0, 255, 0);
+    line(0, -1000, 0, 0, 1000, 0);
+
+    stroke(0, 0, 255);
+    line(0, 0, -1000, 0, 0, 1000);
+
+    stroke(255);
+
+    DrawCubes(grid);
+}
+
+function DrawCubes(grid) {
+    for (let i = 0; i < grid.length; i++) {
+        let x = grid[i][0] * 50;
+        let y = grid[i][1] * 50;
+        let z = grid[i][2] * 50;
+        let active = grid[i][3];
+
+        push();
+
+        if (active === 1) {
+            stroke(0, 152, 94);
+            fill(0, 102, 94);
+        }
+        else {
+            stroke('rgba(100%,0%,0%,0.2)')
+            //fill(255, 102, 94,0.1);
+            fill('rgba(100%,30%,30%,0.01)');
+        }
+        translate(createVector(x, y, z))
+        box(20);
+
+        pop();
+    }
+}
